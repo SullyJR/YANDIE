fcd1478c59d604251563939bc99e055a147ec960
Sen Macmaster <macse341@student.otago.ac.nz>
2023-03-15 10:39:01 +1300

    Added SettingsAction without implementation

---
+++ src/cosc202/andie/SettingsActions.java
@@ -0,0 +1,119 @@
+package cosc202.andie;
+
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * <p>
+ * Actions provided by the Settings menu.
+ * </p>
+ * 
+ * <p>
+ * The Settings menu is very common across applications, 
+ * and there are several items that the user will expect to find here.
+ * For the time being it will just be language support.
+ * </p>
+ * 
+ * <p> 
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * </p>
+ * 
+ * @author Steven Mills
+ * @version 1.0
+ */
+public class SettingsActions {
+    
+    /** A list of actions for the Settings menu. */
+    protected ArrayList<Action> actions;
+
+    /**
+     * <p>
+     * Create a set of Settings menu actions.
+     * </p>
+     */
+    public SettingsActions() {
+        actions = new ArrayList<Action>();
+        actions.add(new LanguageAction("Language", null, "Change language", Integer.valueOf(KeyEvent.VK_O)));
+    }
+
+    /**
+     * <p>
+     * Create a menu containing the list of Settings actions.
+     * </p>
+     * 
+     * @return The Settings menu UI element.
+     */
+    public JMenu createMenu() {
+        JMenu settingsMenu = new JMenu("Settings");
+
+        for(Action action: actions) {
+            settingsMenu.add(new JMenuItem(action));
+        }
+
+        return settingsMenu;
+    }
+
+    /**
+     * <p>
+     * Action to change language from settings.
+     * </p>
+     * 
+     */
+    public class LanguageAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new settings-language action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        LanguageAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the settings-language action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the SettingsLanguageAction is triggered.
+         * It prompts the user to select a language from a checkbox group and will
+         * change all the language from the program
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+
+            // Determine the radius - ask the user.
+            int radius = 1;
+    
+            // Pop-up dialog box to ask for the radius value.
+            SpinnerNumberModel radiusModel = new SpinnerNumberModel(1, 1, 10, 1);
+            JSpinner radiusSpinner = new JSpinner(radiusModel);
+            int option = JOptionPane.showOptionDialog(null, radiusSpinner, "Enter filter radius",
+            JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+    
+            // Check the return value from the dialog box.
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                radius = radiusModel.getNumber().intValue();
+            }
+    
+             // Create and apply the filter
+            target.getImage().apply(new MeanFilter(radius));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+    }
+
+}
+
+
