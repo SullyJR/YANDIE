6a68b17bbc9262e9680b2b19ea5e6e4753fd33be
Andrew John <johan372@student.otago.ac.nz>
2023-03-14 15:50:48 +1300

    Added Sharpen Filter

--- src/cosc202/andie/SharpenFilter.java
+++ src/cosc202/andie/SharpenFilter.java
@@ -1,95 +1,27 @@
 package cosc202.andie;
 
 import java.awt.image.*;
-import java.util.*;
 
-/**
- * <p>
- * ImageOperation to apply a Mean (simple blur) filter.
- * </p>
- * 
- * <p>
- * A Sharpen Filter blurs an image by replacing each pixel by the average of the
- * pixels in a surrounding neighbourhood, and can be implemented by a convoloution.
- * </p>
- * 
- * <p> 
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
- * </p>
- * 
- * @see java.awt.image.ConvolveOp
- * @author Steven Mills
- * @version 1.0
- */
-public class SharpenFilter implements ImageOperation, java.io.Serializable {
-    
-    /**
-     * The size of filter to apply. A radius of 1 is a 3x3 filter, a radius of 2 a 5x5 filter, and so forth.
-     */
-    private int radius;
-
-    /**
-     * <p>
-     * Construct a Sharpen Filter with the given size.
-     * </p>
-     * 
-     * <p>
-     * The size of the filter is the 'radius' of the convolution kernel used.
-     * A size of 1 is a 3x3 filter, 2 is 5x5, and so on.
-     * Larger filters give a stronger blurring effect.
-     * </p>
-     * 
-     * @param radius The radius of the newly constructed SharpenFilter
-     */
-    SharpenFilter(int radius) {
-        this.radius = radius;    
+public class SoftBlur implements ImageOperation, java.io.Serializable {
+    SoftBlur() {
+        // Any construction code goes here
     }
 
-    /**
-     * <p>
-     * Construct a Sharpen Filter with the default size.
-     * </p
-     * >
-     * <p>
-     * By default, a Sharpen Filter has radius 1.
-     * </p>
-     * 
-     * @see SharpenFilter(int)
-     */
-    SharpenFilter() {
-        this(1);
-    }
-
-    /**
-     * <p>
-     * Apply a Sharpen Filter to an image.
-     * </p>
-     * 
-     * <p>
-     * As with many filters, the Sharpen Filter is implemented via convolution.
-     * The size of the convolution kernel is specified by the {@link radius}.  
-     * Larger radii lead to stronger blurring.
-     * </p>
-     * 
-     * @param input The image to apply the Sharpen Filter to.
-     * @return The resulting (blurred)) image.
-     */
     public BufferedImage apply(BufferedImage input) {
-        int size = (2*radius+1) * (2*radius+1);
-        float [] sharpen = new float[] {
-            0.0f, -1.0f, 0.0f,
-            -1.0f, 5.0f, -1.0f,
-            0.0f, -1.0f, 0.0f
-        };
-        Arrays.fill(array, 1.0f/size);
-
-        Kernel kernel = new Kernel(2*radius+1, 2*radius+1, sharpen);
+        float[] array = { 0, 1 / 8.0f, 0,
+                1 / 8.0f, 1 / 2.0f, 1 / 8.0f,
+                0, 1 / 8.0f, 0 };
+        // Make a 3x3 filter from the array
+        Kernel kernel = new Kernel(3, 3, array);
+        // Apply this as a convolution - same code as in MeanFilter
         ConvolveOp convOp = new ConvolveOp(kernel);
-        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
+
+        BufferedImage output = new BufferedImage(input.getColorModel(),
+                input.copyData(null),
+                input.isAlphaPremultiplied(), null);
         convOp.filter(input, output);
-
+        // And we're done
         return output;
+
     }
-
-
 }
