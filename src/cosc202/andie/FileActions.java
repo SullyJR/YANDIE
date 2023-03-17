2de5fcb95b77a3d553f5d45dc20ab53db25dffec
Sam Bugden <bugsa135@student.otago.ac.nz>
2023-03-17 14:31:11 +1300

    Added Error Message

--- src/cosc202/andie/FileActions.java
+++ src/cosc202/andie/FileActions.java
@@ -35,6 +35,7 @@ public class FileActions {
     public FileActions() {
         actions = new ArrayList<Action>();
         actions.add(new FileOpenAction("Open", null, "Open a file", Integer.valueOf(KeyEvent.VK_O)));
+        actions.add(new FileOpenAction("Open Default", null, "Open a Default Image", Integer.valueOf(KeyEvent.VK_D)));
         actions.add(new FileSaveAction("Save", null, "Save the file", Integer.valueOf(KeyEvent.VK_S)));
         actions.add(new FileSaveAsAction("Save As", null, "Save a copy", Integer.valueOf(KeyEvent.VK_A)));
         actions.add(new FileExitAction("Exit", null, "Exit the program", Integer.valueOf(0)));
@@ -101,6 +102,57 @@ public class FileActions {
                     String imageFilepath = fileChooser.getSelectedFile().getCanonicalPath();
                     target.getImage().open(imageFilepath);
                 } catch (Exception ex) {
+                    JPanel error = new JPanel();
+                    error.add(new JLabel("This file type is not Supported"));
+                    error.setVisible(enabled);
+                    JOptionPane.showMessageDialog(target, error, "Error", JOptionPane.ERROR_MESSAGE);
+                    
+                }
+            }
+
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
+    }
+
+    public class FileOpenDefaultAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new file-open action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        FileOpenDefaultAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the file-open action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the FileOpenAction is triggered.
+         * It prompts the user to select a file and opens it as an image.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+           // JFileChooser fileChooser = new JFileChooser();
+            //int result = fileChooser.showOpenDialog(target);
+
+            if (1==1) {
+                try {
+                    //String imageFilepath = fileChooser.getSelectedFile().getCanonicalPath();
+                    target.getImage().open("J:/andie/src/image.jpg");
+                } catch (Exception ex) {
                     System.exit(1);
                 }
             }
@@ -110,6 +162,7 @@ public class FileActions {
         }
 
     }
+
 
     /**
      * <p>
