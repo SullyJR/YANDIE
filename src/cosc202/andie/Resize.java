26d9892bd95c53b9086dec499e5fd20fececbf60
Andrew John <johan372@student.otago.ac.nz>
2023-03-17 13:23:05 +1300

    Fixed Resize Functionally to work properly

--- src/cosc202/andie/Resize.java
+++ src/cosc202/andie/Resize.java
@@ -1,6 +1,7 @@
 package cosc202.andie;
 
 import java.awt.image.*;
+import java.awt.Graphics2D;
 
 /**
  * Image Operation to resize an image accordingly
@@ -31,9 +32,18 @@ public class Resize implements ImageOperation, java.io.Serializable {
     /** applying the resize method to picture */
     public BufferedImage apply(BufferedImage input) {
         
-        int resizeH = (int) percentage * input.getHeight();
-        int resizeW = (int) percentage * input.getWidth();
-        return (BufferedImage) input.getScaledInstance(resizeW, resizeH, input.SCALE_DEFAULT);
+        int resizeH = (int) (percentage * input.getHeight());
+        int resizeW = (int) (percentage * input.getWidth());
+
+        // Create a new BufferedImage with the new dimensions
+        BufferedImage output = new BufferedImage(resizeW, resizeH, input.getType());
+
+        // Scale the input image to the new dimensions
+        Graphics2D g2d = output.createGraphics();
+        g2d.drawImage(input, 0, 0, resizeW, resizeH, null);
+        g2d.dispose();
+
+        return output;
     }
 
 }
