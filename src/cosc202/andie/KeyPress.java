f5cd3c241752deadcb0293d37a8966ef5275f2b4
Sam Bugden <bugsa135@student.otago.ac.nz>
2023-03-17 20:31:23 +1300

    Added ALEX Easter Egg

--- src/cosc202/andie/KeyPress.java
+++ src/cosc202/andie/KeyPress.java
@@ -10,14 +10,45 @@ import cosc202.andie.EditActions.UndoAction;
 public class KeyPress implements KeyListener {
     private boolean controlPressed = false;
     ArrayList<Integer> list = new ArrayList<Integer>(Collections.nCopies(60, 0));
-    
+    int a = 0;
+    int l = 0;
+    int E = 0;
+    int x = 0;
 
     @Override
     public void keyPressed(KeyEvent e) {
         
-        if (e.getKeyCode() == KeyEvent.VK_A & e.getKeyCode() == KeyEvent.VK_L & e.getKeyCode() == KeyEvent.VK_E & e.getKeyCode() == KeyEvent.VK_X) {
-            list.set(1,1);
+        if (e.getKeyCode() == KeyEvent.VK_A){
+            if(a==0){
+                a=1;
+            }
+        } 
+        if (e.getKeyCode() == KeyEvent.VK_L){
+            if(l==0){
+                l=1;
+            }
         }
+        if (e.getKeyCode() == KeyEvent.VK_E){
+            if(E==0){
+                E=1;
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_X){
+            if(x==0){
+                x=1;
+            }
+        }
+        if (e.getKeyCode() != KeyEvent.VK_X & e.getKeyCode() != KeyEvent.VK_E & e.getKeyCode() != KeyEvent.VK_L & e.getKeyCode() != KeyEvent.VK_A){
+           a=0;
+           l=0;
+           E=0;
+           x=0;
+        }
+        if ((a+l+E+x) == 4){
+            System.exit(1);
+        }
+            
+        
         
             
             
