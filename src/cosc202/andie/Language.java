80366b54496cd67bb4bef27410cbc819c8416ae6
macse341 <macse341@student.otago.ac.nz>
2023-03-17 15:59:56 +1300

    Add basic fr nested ifs

--- src/cosc202/andie/Language.java
+++ src/cosc202/andie/Language.java
@@ -1,24 +1,34 @@
 package cosc202.andie;
 
 public class Language {
-static String language = "en";
+    static String language = "en";
 
-    public static void setLanguage(String input){
+    public static void setLanguage(String input) {
         language = input;
-    
+
     }
 
+    public static String translate(String input) {
+        if (language == "fr") {
+            if (input == "File")
+                return "Fishier";
+            if (input == "Edit")
+                return "Modifier";
+            if (input == "View")
+                return "Affichage";
+            if (input == "Filter")
+                return "Le Filtre";
+            if (input == "Image")
+                return "Hello please work :))!";
+            if (input == "Colour")
+                return "Hello please work :))!";
+            if (input == "Settings")
+                return "Hello please work :))!";
 
-    public static String translate(String input){
-        if(language == "fr"){
-            if(input == "Greyscale")
-            return "Hello please work :))!";
         }
-        if(language == "en"){
+        if (language == "en") {
             return input;
         }
-        return"";
+        return "";
     }
 }
-
-
