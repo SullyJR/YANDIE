fe0e57fa80619fe2760481672e27013f8302e070
Sen Macmaster <macse341@student.otago.ac.nz>
2023-03-17 16:35:09 +1300

    Added french nested if to Language

--- src/cosc202/andie/Language.java
+++ src/cosc202/andie/Language.java
@@ -8,25 +8,66 @@ public class Language {
 
     }
 
-    public static String translate(String input){
-        if(language == "en"){
+    public static String translate(String input) {
+        
+        if (language == "fr") {
+            if (input == "File")
+                return "Fichier";
+            if (input == "Edit")
+                return "Édition";
+            if (input == "View")
+                return "Affichage";
+            if (input == "Filter")
+                return "Filtre";
+            if (input == "Image")
+                return "Image";
+            if (input == "Colour")
+                return "Couleur";
+            if (input == "Settings")
+                return "Paramètres";
+            if (input == "Open")
+                return "Ouvrir";
+            if (input == "Save")
+                return "Enregistrer";
+            if (input == "Save as")
+                return "Enregistrer sous";
+            if (input == "Exit")
+                return "Quitter";
+            if (input == "Undo")
+                return "Annuler";
+            if (input == "Redo")
+                return "Rétablir";
+            if (input == "Zoom In")
+                return "Zoom avant";
+            if (input == "Zoom Out")
+                return "Zoom arrière";
+            if (input == "Zoom Full")
+                return "Zoom pleine page";
+            if (input == "Mean")
+                return "Moyenne";
+            if (input == "Soft")
+                return "Doux";
+            if (input == "Sharpen")
+                return "Netteté";
+            if (input == "Gaussian")
+                return "Gaussien";
+            if (input == "Median")
+                return "Médiane";
+            if (input == "Rotate")
+                return "Rotation";
+            if (input == "Resize")
+                return "Redimensionner";
+            if (input == "Greyscale")
+                return "Niveau de gris";
+            if (input == "Brightness")
+                return "Luminosité";
+            if (input == "Language")
+                return "Langue";
+        }
+        if (language == "en") {
             return input;
         }
-
-        if(language == "fr"){
-            if(input == "Greyscale")
-            return "";
-            if(input == "Brightness")
-            return "";
-            if(input == "Zoom in")
-            return "";
-            if(input == "Zoom out")
-            return "";
-            if(input == "Open")
-            return "";
-        }
-
-        
-        return "Error: Language not Found"; //If it gets through the whole code without finding a match, return this (should never happen)
+        return "";
     }
 }
+
