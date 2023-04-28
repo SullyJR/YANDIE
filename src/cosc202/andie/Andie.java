151ea365d6e7c92d942729833e201ff8da755a0b
macse341 <macse341@student.otago.ac.nz>
2023-04-28 15:55:35 +1200

    Added a JToolBar to the frame

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -1,10 +1,15 @@
 package cosc202.andie;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
 import javax.imageio.*;
 import javax.swing.UIManager.*;
 import java.io.*;
+
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 /**
  * <p>
@@ -27,7 +32,7 @@ import java.io.*;
  */
 public class Andie {
 
-    static String[] languages = {"English", "French", "Malay"};
+    static String[] languages = { "English", "French", "Malay" };
     static JFrame frame;
 
     /**
@@ -58,12 +63,30 @@ public class Andie {
      * @throws Exception if something goes wrong.
      */
     public static void createAndShowGUI() throws Exception {
+
+        ArrayList<Action> actions = new ArrayList<Action>();
+
+        ImagePanel ip = new ImagePanel();
+
+        ip.iconArray[0].setImage(ip.iconArray[0].getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH)); // Open
+        ip.iconArray[2].setImage(ip.iconArray[2].getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH)); // Save
+        ip.iconArray[5].setImage(ip.iconArray[5].getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH)); // Undo
+        ip.iconArray[6].setImage(ip.iconArray[6].getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH)); // Redo
+
+        FileActions fActions = new FileActions();
+        EditActions eActions = new EditActions();
+
+        actions.add(fActions.new FileOpenAction(null, ip.iconArray[0], null, Integer.valueOf(KeyEvent.VK_O)));
+        actions.add(fActions.new FileSaveAction(null, ip.iconArray[2], null, Integer.valueOf(KeyEvent.VK_S)));
+        // actions.add(eActions.new UndoAction(null, ip.iconArray[5], null,
+        // Integer.valueOf(KeyEvent.VK_Z)));
+        actions.add(eActions.new RedoAction(null, ip.iconArray[6], null, Integer.valueOf(KeyEvent.VK_Y)));
+
         // Set up the main GUI frame
         frame = new JFrame("ANDIE");
         frame.setForeground(Color.GRAY);
 
-        Image image = ImageIO.read(new File("./src/cosc202/andie/icons/icon.png"));
-        //Image image = ImageIO.read(Andie.class.getClassLoader().getResource("icons/icon.png"));
+        Image image = ImageIO.read(new File("./src/cosc202/andie/icons/icon.png")); // andie icon
 
         frame.setIconImage(image);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,7 +116,11 @@ public class Andie {
         JMenuBar menuBar = new JMenuBar();
         menuBar.setBackground(Color.GRAY);
 
-        // File menus are pretty standard, so things that usually go in File menus go here.
+        // Add in toolbar for easier access to commonly used actions
+        JToolBar toolBar = new JToolBar();
+
+        // File menus are pretty standard, so things that usually go in File menus go
+        // here.
         FileActions fileActions = new FileActions();
         menuBar.add(fileActions.createMenu());
 
@@ -123,16 +150,28 @@ public class Andie {
         SettingsActions settingsActions = new SettingsActions();
         menuBar.add(settingsActions.createMenu());
 
-        //Sets the frame
+        // Sets the frame
         frame.setJMenuBar(menuBar);
+        frame.add(toolBar, BorderLayout.PAGE_START);
         frame.addKeyListener(new KeyPress());
         frame.pack();
         frame.setVisible(true);
 
-        //Sets the menubar
+        // Toolbar buttons
+        toolBar.add(new JButton(null, ip.iconArray[0]));
+        toolBar.add(new JButton(null, ip.iconArray[2]));
+        toolBar.add(new JButton(null, ip.iconArray[5]));
+        toolBar.add(new JButton(null, ip.iconArray[6]));
+
+        // Sets the menubar
         Dimension menuBarSize = new Dimension(400, 35);
         menuBar.setPreferredSize(menuBarSize);
         menuBar.setBackground(Color.GRAY);
+
+        // Sets the toolbar
+        Dimension toolBarSize = new Dimension(400, 18);
+        toolBar.setPreferredSize(toolBarSize);
+        // toolBar.setBackground(Color.GRAY);
 
     }
 
@@ -156,7 +195,7 @@ public class Andie {
 
                 try {
                     createAndShowGUI();
-                    //FileActions.FileOpenDefaultAction();
+                    // FileActions.FileOpenDefaultAction();
 
                 } catch (Exception ex) {
                     ex.printStackTrace();
