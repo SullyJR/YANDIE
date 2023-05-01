90f45de1d5bedac4204007c92b74c89e71daaa47
johan372 <johan372@student.otago.ac.nz>
2023-05-01 17:49:58 +1200

    New Changes to Selection(Not completed yet)

---
+++ src/cosc202/andie/SelectionRectangle.java
@@ -0,0 +1,18 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class SelectionRectangle implements ImageOperation {
+  
+  private ImagePanel ip;
+
+  public SelectionRectangle() {
+
+  }
+
+  public BufferedImage apply(BufferedImage input) {
+    
+  }
+}
