5b615c7fdb70d5ac2b061e1280f75deefa0533c5
bugsa135 <bugsa135@student.otago.ac.nz>
2023-05-05 14:58:38 +1200

    Mouse can now dynamically move around

--- src/cosc202/andie/MousePanel.java
+++ src/cosc202/andie/MousePanel.java
@@ -1,90 +1,58 @@
 package cosc202.andie;
 
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.*;
+import javax.swing.JPanel;
 
-public class MousePanel extends JPanel implements MouseListener{
+public class MousePanel extends JPanel {
 
     private Point anchor;
     private Point anchorEND;
     private Rectangle selection;
-    private boolean down = false;
+
     public MousePanel() {
-        
-        addMouseListener(this);
-    }
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                anchor = e.getPoint();
+                anchorEND = null;
+                repaint();
+            }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        down = true;
-        anchor = e.getPoint();
-        while(down){
-            anchorEND = e.getPoint();        
-            int x = Math.min(anchor.x, e.getX());
-            int y = Math.min(anchor.y, e.getY());
-            int width = Math.abs(e.getX() - anchor.x);
-            int height = Math.abs(e.getY() - anchor.y);
-            selection = new Rectangle(x, y, width, height);
-            System.out.println(selection.getSize());
-            repaint();
-          }
-
-    }
-
-    public double getMX() {
-        return anchor.x;
-    }
-
-    public double getMY() {
-        return anchor.y;
-    }
-
-    public Rectangle getSelection() {
-        return selection;
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        down = false;
-        System.out.println("Mouse released");
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                anchorEND = e.getPoint();
+                repaint();
+            }
+        });
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                anchorEND = e.getPoint();
+                repaint();
+            }
+        });
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if(selection!= null) {
+        if (anchor != null && anchorEND != null) {
+            int x = Math.min(anchor.x, anchorEND.x);
+            int y = Math.min(anchor.y, anchorEND.y);
+            int width = Math.abs(anchorEND.x - anchor.x);
+            int height = Math.abs(anchorEND.y - anchor.y);
+            selection = new Rectangle(x, y, width, height);
             g.setColor(Color.black);
             g.drawRect(selection.x, selection.y, selection.width, selection.height);
         }
-    }  
-
-    /**
-     * Not used yet idk what to do with em (ANDRU)
-     * 
-     */
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stubjohan372
     }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
+    public Rectangle getSelection() {
+        return selection;
     }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-    }
-    
-    
-
-
 }
